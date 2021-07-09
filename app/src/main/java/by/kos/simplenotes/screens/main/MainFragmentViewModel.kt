314143efe33fa4618bfa1063b76554818f27e771
Eugene Kos <mstrsid@live.com>
2021-07-09 09:42:47 +0300

    feat: add transition to addnotefragment

---
+++ app/src/main/java/by/kos/simplenotes/screens/main/MainFragmentViewModel.kt
@@ -0,0 +1,7 @@
+package by.kos.simplenotes.screens.main
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainFragmentViewModel(application: Application): AndroidViewModel(application) {
+}
